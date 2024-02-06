29ce3589e0f126b42e6687b230810a7e9287c65d
Farouk KIOUS <faroukious0@gmail.com>
2024-02-06 21:12:04 +0100

    navigation design

--- resources/js/Components/App/Navigation.vue
+++ resources/js/Components/App/Navigation.vue
@@ -1,9 +1,27 @@
 <script setup>
-
+import {Link} from '@inertiajs/vue3'
+import ApplicationLogo from '@/Components/ApplicationLogo.vue'
+import NavLink from '@/Components/NavLink.vue'
+import CreateNewDropdown from '@/Components/App/CreateNewDropdown.vue'
 </script>
 
 <template>
-    <div>
-        Navigation
-    </div>
+    <nav class=" min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link href="/dashboard">
+                <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800"/>
+            </Link>
+            FRK Drive
+        </div>
+        <div class="px-3">
+            <CreateNewDropdown/>
+            <div class="py-3">
+                <NavLink href="/dashboard">My Files</NavLink>
+                <NavLink href="/dashboard">Shared with me</NavLink>
+                <NavLink href="/dashboard">Shared by me</NavLink>
+                <NavLink href="/dashboard">Starred Files</NavLink>
+                <NavLink href="/dashboard">Trash</NavLink>
+            </div>
+        </div>
+    </nav>
 </template>

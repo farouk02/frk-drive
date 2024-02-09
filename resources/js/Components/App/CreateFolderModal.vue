e8a091dd32aba8b9eb050172f5df36c37b684f06
Farouk KIOUS <faroukious0@gmail.com>
2024-02-09 23:00:05 +0100

    ui/create folder modal

--- resources/js/Components/App/CreateFolderModal.vue
+++ resources/js/Components/App/CreateFolderModal.vue
@@ -1,11 +1,30 @@
 <template>
     <Modal :show="props.modelValue">
-        farouk
+        <div class="p-6">
+            <h2 class="text-lg font-medium text-gray-900">
+                Create new folder
+            </h2>
+            <div class="mt-6">
+                <InputLabel for="folderName" value="folderName" />
+                <TextInput type="text" id="folderName" v-model="form.name" class="mt-1 block w-full"
+                    :class="form.errors.name ? ' border-red-500 focus:border-red-500 focus:ring-red-500' : ''"
+                    placeholder="Folder name" @keyup.enter="createFolder" />
+                <InputError :message="form.errors.name" class="mt-2" />
+            </div>
+            <div class="mt-6 flex justify-end">
+                <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+            </div>
+        </div>
     </Modal>
 </template>
 
 <script setup>
 import Modal from '@/Components/Modal.vue'
+import InputLabel from '../InputLabel.vue';
+import TextInput from '../TextInput.vue';
+import InputError from '../InputError.vue';
+import { useForm } from '@inertiajs/vue3';
+import SecondaryButton from '../SecondaryButton.vue';
 
 const props = defineProps({
     modelValue: {
@@ -14,5 +33,22 @@ const props = defineProps({
     },
 });
 
-console.log(props.modelValue)
+const emit = defineEmits(['update:modelValue'])
+
+const form = useForm({
+    name: ''
+})
+
+
+function createFolder() {
+    console.log('create folder')
+
+}
+
+function closeModal() {
+    emit('update:modelValue');
+    form.clearErrors();
+    form.reset();
+}
+
 </script>
